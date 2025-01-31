2f7be86656ef0ca10a0883d9123ecb4423c3553c
idekwat <184945414+idekwat@users.noreply.github.com>
2025-01-31 15:41:38 +0800

    Created index file, added buttons, and declared needed variables

---
+++ js-scripts.js
@@ -0,0 +1,23 @@
+
+//script here
+
+let num1 = 0.0;
+let num2 = 0.0;
+let num3 = 0.0;
+
+const calc1 = document.getElementById("num1");
+const calc2 = document.getElementById("num2");
+const calc3 = document.getElementById("num3");
+const calc4 = document.getElementById("num4");
+const calc5 = document.getElementById("num5");
+const calc6 = document.getElementById("num6");
+const calc7 = document.getElementById("num7");
+const calc8 = document.getElementById("num8");
+const calc9 = document.getElementById("num9");
+const calc0 = document.getElementById("num0");
+const calcAdd = document.getElementById("toAdd");
+const calcSubt = document.getElementById("toSubt");
+const calcMult = document.getElementById("toMult");
+const calcDiv = document.getElementById("toDiv");
+const calcEql = document.getElementById("toEql");
+
