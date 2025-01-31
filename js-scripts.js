0c3ef1859d028108aa3ec05f95bff0b4dadff003
idekwat <184945414+idekwat@users.noreply.github.com>
2025-01-31 18:08:55 +0800

    added code for arithmetic functions

--- js-scripts.js
+++ js-scripts.js
@@ -4,6 +4,9 @@
 let num1 = 0.0;
 let num2 = 0.0;
 let num3 = 0.0;
+let currentinput = "";
+let operator = "";
+let firstNum = true;
 
 const calc1 = document.getElementById("num1");
 const calc2 = document.getElementById("num2");
@@ -21,3 +24,125 @@ const calcMult = document.getElementById("toMult");
 const calcDiv = document.getElementById("toDiv");
 const calcEql = document.getElementById("toEql");
 
+calc1.onclick = () => {
+    currentinput = currentinput+ "1";
+    console.log(currentinput);
+}
+calc2.onclick = () => {
+    currentinput = currentinput+ "2";
+    console.log(currentinput);
+}
+calc3.onclick = () => {
+    currentinput = currentinput+ "3";
+    console.log(currentinput);
+}
+calc4.onclick = () => {
+    currentinput = currentinput+ "4";
+    console.log(currentinput);
+}
+calc5.onclick = () => {
+    currentinput = currentinput+ "5";
+    console.log(currentinput);
+}
+calc6.onclick = () => {
+    currentinput = currentinput+ "6";
+    console.log(currentinput);
+}
+calc7.onclick = () => {
+    currentinput = currentinput+ "7";
+    console.log(currentinput);
+}
+calc8.onclick = () => {
+    currentinput = currentinput+ "8";
+    console.log(currentinput);
+}
+calc9.onclick = () => {
+    currentinput = currentinput+ "9";
+    console.log(currentinput);
+}
+calc0.onclick = () => {
+    currentinput = currentinput+ "0";
+    console.log(currentinput);
+}
+
+calcAdd.onclick = () => {
+    if(firstNum){
+        num1 = parseFloat(currentinput);
+        currentinput = "";
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        firstNum = false;
+    }
+    operator = "+";
+}
+calcSubt.onclick = () => {
+    if(firstNum){
+        num1 = parseFloat(currentinput);
+        currentinput = "";
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        firstNum = false;
+    }
+    operator = "-";
+}
+calcMult.onclick = () => {
+    if(firstNum){
+        num1 = parseFloat(currentinput);
+        currentinput = "";
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        firstNum = false;
+    }
+    operator = "*";
+}
+calcDiv.onclick = () => {
+    if(firstNum){
+        num1 = parseFloat(currentinput);
+        currentinput = "";
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        firstNum = false;
+    }
+    operator = "/";
+}
+
+calcEql.onclick = () => {
+    switch(operator) {
+        case("+"):
+        num2 = parseFloat(currentinput);
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        num3 = num1 + num2;
+        console.log(num3);
+        currentinput = num3;
+        break;
+
+        case("-"):
+        num2 = parseFloat(currentinput);
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        num3 = num1 - num2;
+        console.log(num3);
+        currentinput = num3;
+        break;
+
+        case("*"):
+        num2 = parseFloat(currentinput);
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        num3 = num1 * num2;
+        console.log(num3);
+        currentinput = num3;
+        break;
+      
+        case("/"):
+        num2 = parseFloat(currentinput);
+        console.log("num1 " + num1);
+        console.log("current " + currentinput);
+        num3 = num1 / num2;
+        console.log(num3);
+        currentinput = num3;
+        break;  
+    }
+    firstNum = true;
+}
