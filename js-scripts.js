9c5f4962a6b6c2c3cbb181c928a7c977e1e0b9e0
idekwat <184945414+idekwat@users.noreply.github.com>
2025-02-01 23:43:48 +0800

    Added js code to do basic operations, fixed index classes and ids

--- js-scripts.js
+++ js-scripts.js
@@ -3,7 +3,9 @@
 
 let num1 = "";
 let num2 = "";
+let num3 = 0.0;
 let operator = "";
+let firstNum = true;
 
 const btn1 = document.getElementById("num1");
 const btn2 = document.getElementById("num2");
@@ -22,6 +24,105 @@ const calcDiv = document.getElementById("divide");
 const calcEql = document.getElementById("equals");
 const clearEverything = document.getElementById("clear");
 
-function operate() {
 
+btn1.onclick = () => {
+    if(firstNum){     num1 += "1";    console.log("num1: " + num1);}
+    else {              num2 += "1";    console.log("num2: " + num2);}
+}
+
+btn2.onclick = () => {
+    if(firstNum){     num1 += "2";    console.log("num1: " + num1);}
+    else {              num2 += "2";    console.log("num2: " + num2);}
+}
+
+btn3.onclick = () => {
+    if(firstNum){     num1 += "3";    console.log("num1: " + num1);}
+    else {              num2 += "3";    console.log("num2: " + num2);}
+}
+
+btn4.onclick = () => {
+    if(firstNum){     num1 += "4";    console.log("num1: " + num1);}
+    else {              num2 += "4";    console.log("num2: " + num2);}
+}
+
+btn5.onclick = () => {
+    if(firstNum){     num1 += "5";    console.log("num1: " + num1);}
+    else {              num2 += "5";    console.log("num2: " + num2);}
+}
+
+btn6.onclick = () => {
+    if(firstNum){     num1 += "6";    console.log("num1: " + num1);}
+    else {              num2 += "6";    console.log("num2: " + num2);}
+}
+
+btn7.onclick = () => {
+    if(firstNum){     num1 += "7";    console.log("num1: " + num1);}
+    else {              num2 += "7";    console.log("num2: " + num2);}
+}
+
+btn8.onclick = () => {
+    if(firstNum){     num1 += "8";    console.log("num1: " + num1);}
+    else {              num2 += "8";    console.log("num2: " + num2);}
+}
+
+btn9.onclick = () => {
+    if(firstNum){     num1 += "9";    console.log("num1: " + num1);}
+    else {              num2 += "9";    console.log("num2: " + num2);}
+}
+
+btn0.onclick = () => {
+    if(firstNum){     num1 += "0";    console.log("num1: " + num1);}
+    else {              num2 += "0";    console.log("num2: " + num2);}
+}
+
+calcAdd.onclick = () => {
+    operator = "+"
+    firstNum = false;
+}
+
+calcSubt.onclick = () => {
+    operator = "-"
+    firstNum = false;
+}
+
+calcMult.onclick = () => {
+    operator = "x"
+    firstNum = false;
+}
+
+calcDiv.onclick = () => {
+    operator = "/"
+    firstNum = false;
+}
+calcEql.onclick = () => {
+    if(num2 == "0" & operator == "/") {
+        alert("NO.");
+    }
+    else {
+        toCalc();
+    }
+}
+
+function toCalc() {
+    switch(operator) {
+        case("+"):
+        num3 = parseFloat(num1) + parseFloat(num2);
+        console.log("Equals: " + num3);
+        break;
+
+        case("-"):
+        num3 = parseFloat(num1) - parseFloat(num2);
+        console.log("Equals: " + num3);
+        break;
+
+        case("x"):
+        num3 = parseFloat(num1) * parseFloat(num2);
+        console.log("Equals: " + num3);
+        break;
+
+        case("/"):
+        num3 = parseFloat(num1) / parseFloat(num2);
+        console.log("Equals: " + num3);
+        break;
+    }
 }
